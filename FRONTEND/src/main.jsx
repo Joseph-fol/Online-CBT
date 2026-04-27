632e98b9769cfc87de4db14ac03f9967385cb0e5
Olawoyin Joseph Folasakin <olawoyinjoseph05@gmail.com>
2026-04-27 13:02:48 +0100

    feat: add PWA support and update manifest configuration
    - Added vite-plugin-pwa to package.json for PWA capabilities.
    - Included new icons for PWA in the public directory.
    - Updated main.jsx to register the service worker for PWA.
    - Configured vite.config.js to include PWA settings and manifest details.

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -2,34 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
 import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// // import ActiveQuizView from './student/ActiveQuizView.jsx'
-// import LandingPage from './component/LandingPage.jsx'
-// import SignupPage from './student/SignupPage.jsx'
-// import SigninPage from './student/SigninPage.jsx'
-// import StudentDashboard from './student/StudentDashboard.jsx'
-// import AssignedObject from './student/AssignedObject.jsx'
-// import ResultAndHistory from './student/ResultAndHistory.jsx'
-// import ForgotPassword from './student/ForgotPassword.jsx'
-// import AdminSignin from './admin/AdminDashboard.jsx'
-// import AdminDashboard from './admin/AdminDashboard.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import { registerSW } from 'virtual:pwa-register'
+registerSW({immediate: true}) 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <SignupPage/> */}
-    {/* <SigninPage/> */}
-    {/* <LandingPage/> */}
-      {/* <ActiveQuizView/> */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      {/* <ResultAndHistory/> */}
-      {/* <ForgotPassword/> */}
-      {/* <AdminSignin/> */}
-      {/* <AdminDashboard/> */}
   </StrictMode>,
 )
--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.jpg'],
+      manifest: {
+        name: 'Online CBT',
+        short_name: 'OCBT',
+        description: 'Online CBT is a fast, secure, and mobile-friendly computer-based testing platform that lets students take assessments anytime, while giving admins tools to manage questions, subjects, and results efficiently.',
+        start_url: '/',
+        scope: '/',
+        theme_color: '#ab3500',
+        background_color: '#fbf3ed',
+        display: 'standalone',
+        icons: [
+          {
+            src:'/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src:'/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      },
+
+      devOptions: {
+        enabled: true,
+      }
+    })
+  ],
 })
